a79ab05fea52a24debbbf3bd715ec216f287231c
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-02-02 10:53:51 +0800

    add configuration manager component

---
+++ core/src/main/java/cn/lunadeer/dominion/utils/configuration/ConfigurationPart.java
@@ -0,0 +1,9 @@
+package cn.lunadeer.dominion.utils.configuration;
+
+/**
+ * Marker interface for unique sections of a configuration.
+ * <p>
+ * The items in the configuration part should be public fields.
+ */
+public abstract class ConfigurationPart {
+}
