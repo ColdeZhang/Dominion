6d203f7fbe5cf73c44f31cca98403301bd47651a
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-02-12 23:01:10 +0800

    fix wrong economy handler

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ var libraries = listOf<String>()
 libraries = libraries + "cn.lunadeer:MinecraftPluginUtils:2.0.7"
 
 group = "cn.lunadeer"
-version = "3.5.0-alpha.2"
+version = "3.5.1-beta"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
