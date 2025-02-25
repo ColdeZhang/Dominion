460867d41d73db227ad9b7719a058e91cb93c1b5
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-02-25 11:57:25 +0800

    add placeholder api docs, install&config

--- core/src/main/java/cn/lunadeer/dominion/managers/PlaceHolderApi.java
+++ core/src/main/java/cn/lunadeer/dominion/managers/PlaceHolderApi.java
@@ -3,6 +3,7 @@ package cn.lunadeer.dominion.managers;
 import cn.lunadeer.dominion.Cache;
 import cn.lunadeer.dominion.api.dtos.DominionDTO;
 import cn.lunadeer.dominion.api.dtos.GroupDTO;
+import me.clip.placeholderapi.PlaceholderAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,12 +13,19 @@ public class PlaceHolderApi extends PlaceholderExpansion {
 
     private final JavaPlugin plugin;
 
-    public static PlaceHolderApi instance;
+    public static PlaceHolderApi instance = null;
 
     public PlaceHolderApi(JavaPlugin plugin) {
         this.plugin = plugin;
         this.register();
         instance = this;
+    }
+
+    public static String setPlaceholders(Player player, String text) {
+        if (instance == null) {
+            return text;
+        }
+        return PlaceholderAPI.setPlaceholders(player, text);
     }
 
     @Override
