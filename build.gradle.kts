af8b81b3ad4a3d989c3122bdbd455d7e27161320
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-03-16 22:21:39 +0800

    fix won't check player status when cross board

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ libraries += "net.kyori:adventure-platform-bukkit:4.3.3"
 
 
 group = "cn.lunadeer"
-version = "4.0.0-alpha.21"
+version = "4.0.0-alpha.22"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
