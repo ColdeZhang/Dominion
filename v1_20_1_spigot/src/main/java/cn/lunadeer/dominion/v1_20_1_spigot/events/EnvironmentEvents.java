b208667992c9e45ebfacfaadece508ba2f371bc0
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-03-20 17:17:26 +0800

    optimize event class loader

--- v1_20_1_spigot/src/main/java/cn/lunadeer/dominion/v1_20_1_spigot/events/EnvironmentEvents.java
+++ v1_20_1_spigot/src/main/java/cn/lunadeer/dominion/v1_20_1_spigot/events/EnvironmentEvents.java
@@ -5,6 +5,7 @@ import cn.lunadeer.dominion.api.dtos.DominionDTO;
 import cn.lunadeer.dominion.api.dtos.flag.Flags;
 import cn.lunadeer.dominion.cache.CacheManager;
 import cn.lunadeer.dominion.utils.XLogger;
+import cn.lunadeer.dominion.utils.scheduler.CancellableTask;
 import cn.lunadeer.dominion.utils.scheduler.Scheduler;
 import org.bukkit.Location;
 import org.bukkit.Tag;
@@ -427,41 +428,38 @@ public class EnvironmentEvents implements Listener {
         }
     }
 
-    static {
-        ConcurrentHashMap<UUID, Location> entityMap = new ConcurrentHashMap<>();
-        Scheduler.runTaskRepeat(() -> {
-            Dominion.instance.getServer().getWorlds().forEach(world -> {
-                world.getEntities().forEach(entity -> {
-                    if (!(entity instanceof LivingEntity)) {
-                        return;
-                    }
-                    if (!entityMap.containsKey(entity.getUniqueId())) {
-                        entityMap.put(entity.getUniqueId(), entity.getLocation());
+    private static final ConcurrentHashMap<UUID, Location> entityMap = new ConcurrentHashMap<>();
+    public static CancellableTask entityMoveTask = Scheduler.runTaskRepeat(() -> {
+        Dominion.instance.getServer().getWorlds().forEach(world -> {
+            world.getEntities().forEach(entity -> {
+                if (!(entity instanceof LivingEntity)) {
+                    return;
+                }
+                if (!entityMap.containsKey(entity.getUniqueId())) {
+                    entityMap.put(entity.getUniqueId(), entity.getLocation());
+                } else {
+                    Location lastLoc = entityMap.get(entity.getUniqueId());
+                    Location currentLoc = entity.getLocation();
+                    DominionDTO dom = CacheManager.instance.getDominion(currentLoc);
+                    if (!checkEnvironmentFlag(dom, Flags.ANIMAL_MOVE, null) && entity instanceof Animals) {
+                        entity.teleport(lastLoc);
+                    } else if (!checkEnvironmentFlag(dom, Flags.MONSTER_MOVE, null) && entity instanceof Monster) {
+                        entity.teleport(lastLoc);
                     } else {
-                        Location lastLoc = entityMap.get(entity.getUniqueId());
-                        Location currentLoc = entity.getLocation();
-                        DominionDTO dom = CacheManager.instance.getDominion(currentLoc);
-                        if (!checkEnvironmentFlag(dom, Flags.ANIMAL_MOVE, null) && entity instanceof Animals) {
-                            entity.teleport(lastLoc);
-                        } else if (!checkEnvironmentFlag(dom, Flags.MONSTER_MOVE, null) && entity instanceof Monster) {
-                            entity.teleport(lastLoc);
-                        } else {
-                            entityMap.put(entity.getUniqueId(), currentLoc);
-                        }
+                        entityMap.put(entity.getUniqueId(), currentLoc);
                     }
-                });
-            });
-        }, 0, 30);
-
-        Scheduler.runTaskRepeat(() -> {
-            entityMap.forEach((uuid, loc) -> {
-                Entity e = Dominion.instance.getServer().getEntity(uuid);
-                if (e == null || e.isDead()) {
-                    entityMap.remove(uuid);
                 }
             });
-        }, 0, 6000);
-    }
+        });
+    }, 20, 30);
+    public static CancellableTask entityMoveCleanTask = Scheduler.runTaskRepeat(() -> {
+        entityMap.forEach((uuid, loc) -> {
+            Entity e = Dominion.instance.getServer().getEntity(uuid);
+            if (e == null || e.isDead()) {
+                entityMap.remove(uuid);
+            }
+        });
+    }, 20, 6000);
 
     private static final Map<UUID, Location> fallingBlockMap = new java.util.HashMap<>();
 }
