34cc9e3025c89c32526c932670de46af2be42210
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-04-01 08:52:25 +0800

    fix player current dominion cache null exception

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ libraries += "com.zaxxer:HikariCP:6.2.1"
 
 
 group = "cn.lunadeer"
-version = "4.0.0-alpha.34"
+version = "4.0.0-alpha.35"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
