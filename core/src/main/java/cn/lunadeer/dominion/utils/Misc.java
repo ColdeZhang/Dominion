22ac4f820338e73fc531abaddf01f49f0a89e85e
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-04-09 09:20:11 +0800

    optimize some codes

--- core/src/main/java/cn/lunadeer/dominion/utils/Misc.java
+++ core/src/main/java/cn/lunadeer/dominion/utils/Misc.java
@@ -1,5 +1,12 @@
 package cn.lunadeer.dominion.utils;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Misc {
 
     public static boolean isPaper() {
@@ -20,6 +27,37 @@ public class Misc {
             formatStr = formatStr.replace("{" + i + "}", args[i].toString());
         }
         return formatStr;
+    }
+
+    public static List<String> listClassOfPackage(JavaPlugin plugin, String packageName) {
+        List<String> classesInPackage = new ArrayList<>();
+        // list all classes in the packageName package
+        String path = packageName.replace('.', '/');
+        URL packageDir = plugin.getClass().getClassLoader().getResource(path);
+        if (packageDir == null) {
+            return classesInPackage;
+        }
+        String packageDirPath = packageDir.getPath();
+        // if the package is in a jar file, unpack it and list the classes
+        packageDirPath = packageDirPath.substring(0, packageDirPath.indexOf("jar!") + 4);
+        packageDirPath = packageDirPath.replace("file:", "");
+        packageDirPath = packageDirPath.replace("!", "");
+        // unpack the jar file
+        XLogger.debug("Unpacking class in jar: {0}", packageDirPath);
+        File jarFile = new File(packageDirPath);
+        if (!jarFile.exists() || !jarFile.isFile()) {
+            XLogger.debug("Skipping {0} because it is not a jar file", packageDirPath);
+            return classesInPackage;
+        }
+        // list the classes in the jar file
+        try (java.util.jar.JarFile jar = new java.util.jar.JarFile(jarFile)) {
+            jar.stream().filter(entry -> entry.getName().endsWith(".class") && entry.getName().startsWith(path))
+                    .forEach(entry -> classesInPackage.add(entry.getName().replace('/', '.').substring(0, entry.getName().length() - 6)));
+        } catch (Exception e) {
+            XLogger.debug("Failed to list classes in jar: {0}", e.getMessage());
+            return classesInPackage;
+        }
+        return classesInPackage;
     }
 
 }
